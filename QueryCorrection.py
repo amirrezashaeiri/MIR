1474bda0aea2a0f7cda9fbad31a2f53709d5638c
Amirreza Shaeiri <61947324+amirrezashaeiri@users.noreply.github.com>
2020-11-17 12:14:00 +0330

    Update QueryCorrection.py

--- QueryCorrection.py
+++ QueryCorrection.py
@@ -1,1 +1,114 @@
 
+def helpPreProcess(string, language):
+    
+    res = ""
+    if language == "persian":
+        res = string_preProcess_persian(string)   
+    else if language == "english":
+        res = string_preProcess_english(string)
+        
+    return res
+
+  
+def helpIndex(language, bigram):
+    
+    dic = None
+    if language == "persian" and bigram:
+        dic = None  
+    else if language == "english" and bigram:
+        dic = None
+    else if language == "persian":
+        dic = None
+    else if language == "english":
+        dic = None
+        
+    return dic
+
+
+def stringCorrection(string, language, ktop=10):
+    
+    string_split = string.split()
+    
+    res = ""
+    for x in string_split:
+        
+        xp = helpPreProcess(x, language)
+        
+        if len(xp) == 0 or len(x) = 1:
+            res = res + " " + x
+        else:
+            xc = wordCorrection(xp, helpIndex(language, True), ktop)
+            if xc == xp:
+                res = res + " " + x
+            else:
+                res = res + " " + xc
+                
+    res = res[1:]
+    
+    return res
+    
+    
+def wordCorrection(word, dic, ktop):
+    
+    bigram, n_word = makeBigram(word)
+    
+    temp = []
+    for x in bigram:
+        temp += dic[x]
+    words_dic = {x:temp.count(x) for x in temp}
+    
+    words_jacard_dic = {}
+    for x, y in words_dic.items():
+        _, temp_n = makeBigram(x)
+        words_jacard_dic[x] = Jacard(n_word, temp_n, y)
+    
+    words_jacard_sorted = sorted(words_jacard_dic.items(), key=lambda item: item[1], reverse=True)
+    
+    words_jacard_top = words_jacard_sorted[:ktop]
+    
+    minimum = 1000
+    correct = ""
+    for x, _ in words_jacard_top:
+        temp = levenshteinDistance(word, x)
+        if temp < minimum:
+            minimum = temp
+            correct = x
+    
+    return correct
+    
+    
+def makeBigram(word):
+    
+    bigram = []
+    for i in range(len(word) - 1):
+        bigram += [word[i:i + 2]]
+        
+    n_word = len(bigram)
+    
+    return bigram, n_word
+
+
+def Jacard(A, B, AB):
+    res = AB / (A + B - AB)
+    return res
+    
+    
+def levenshteinDistance(s1, s2):
+    
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        temp_distances = [i2 + 1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                temp_distances.append(distances[i1])
+            else:
+                temp_distances.append(1 + min((distances[i1], distances[i1 + 1], temp_distances[-1])))
+        distances = temp_distances
+    res = distances[-1]
+    
+    return res
+  
+  
